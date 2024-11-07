7529762b21ac959cad51f2fdae87b8297a0e4364
eros-Matip <eros.matip@gmail.com>
2024-11-07 19:12:52 +0100

    adding 5MB to 10MB from Multer

--- src/middlewares/Multer.ts
+++ src/middlewares/Multer.ts
@@ -1,37 +1,43 @@
-import { randomBytes } from 'crypto';
-import { Options, diskStorage } from 'multer';
+import { randomBytes } from "crypto";
+import { Options, diskStorage } from "multer";
 
 export const multerConfig = {
-    storage: diskStorage({
-        filename: (req, file, callback) => {
-            const formatsImage = ['image/jpeg', 'image/jpg', 'image/png'];
-            randomBytes(16, (error, hash) => {
-                if (error) {
-                    callback(error, file.filename);
-                }
-                if (file.mimetype.includes('application/pdf')) {
-                    const applicationFilename = `${hash.toString('hex')}${file.mimetype.replace('application/', '.')}`;
-                    callback(null, applicationFilename);
-                } else if (formatsImage.includes(file.mimetype)) {
-                    const imageFilename = `${hash.toString('hex')}${file.mimetype.replace('image/', '.')}`;
-                    callback(null, imageFilename);
-                } else {
-                    const audioFilename = `${hash.toString('hex')}${file.mimetype.replace('audio/', '.')}`;
-                    callback(null, audioFilename);
-                }
-            });
+  storage: diskStorage({
+    filename: (req, file, callback) => {
+      const formatsImage = ["image/jpeg", "image/jpg", "image/png"];
+      randomBytes(16, (error, hash) => {
+        if (error) {
+          callback(error, file.filename);
         }
-    }),
-    limits: {
-        fileSize: 5 * 1024 * 1024 //5MB
-    },
-
-    fileFilter: (req, file, callback) => {
-        const formats = ['image/jpeg', 'image/jpg', 'image/png', 'application/pdf', 'audio/mpeg'];
-        if (formats.includes(file.mimetype)) {
-            callback(null, true);
+        if (file.mimetype.includes("application/pdf")) {
+          const applicationFilename = `${hash.toString("hex")}${file.mimetype.replace("application/", ".")}`;
+          callback(null, applicationFilename);
+        } else if (formatsImage.includes(file.mimetype)) {
+          const imageFilename = `${hash.toString("hex")}${file.mimetype.replace("image/", ".")}`;
+          callback(null, imageFilename);
         } else {
-            callback(new Error('Format not accepted'));
+          const audioFilename = `${hash.toString("hex")}${file.mimetype.replace("audio/", ".")}`;
+          callback(null, audioFilename);
         }
+      });
+    },
+  }),
+  limits: {
+    fileSize: 10 * 1024 * 1024, //5MB
+  },
+
+  fileFilter: (req, file, callback) => {
+    const formats = [
+      "image/jpeg",
+      "image/jpg",
+      "image/png",
+      "application/pdf",
+      "audio/mpeg",
+    ];
+    if (formats.includes(file.mimetype)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Format not accepted"));
     }
+  },
 } as Options;
