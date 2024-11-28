2d73aae91afdce7339ec727d1eeee67125c95af8
eros-Matip <eros.matip@gmail.com>
2024-11-28 13:23:50 +0100

    adding apple singIn

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+declare module "jsonwebtoken" {
+  const decode: (token: string, options?: any) => any;
+  const sign: (payload: any, secret: string, options?: any) => string;
+  const verify: (
+    token: string,
+    secret: string,
+    options?: any
+  ) => { [key: string]: any };
+  export { decode, sign, verify };
+}
--- src/routes/LoginBySocial.ts
+++ src/routes/LoginBySocial.ts
@@ -1,24 +1,26 @@
 import express, { Request, Response } from "express";
 import Customer from "../models/Customer";
 import axios from "axios";
+import jwt from "jsonwebtoken"; // Pour décoder le token d'Apple
 const uid2 = require("uid2");
 import Retour from "../library/Retour";
 
 const router = express.Router();
 
 router.post("/socialLogin", async (req: Request, res: Response) => {
-  const { provider, accessToken } = req.body;
+  const { provider, accessToken, idToken } = req.body;
 
-  if (!provider || !accessToken) {
+  if (!provider || (!accessToken && !idToken)) {
+    Retour.error("Provider and accessToken or idToken are required");
     return res
       .status(400)
-      .json({ message: "Provider and accessToken are required" });
+      .json({ message: "Provider and accessToken or idToken are required" });
   }
 
   try {
-    let userData;
+    let userData: any;
 
-    // Vérification du token en fonction du provider (Google ou Facebook)
+    // Vérification du token en fonction du provider (Google, Facebook ou Apple)
     if (provider === "google") {
       const googleResponse = await axios.get(
         `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`
@@ -29,7 +31,33 @@ router.post("/socialLogin", async (req: Request, res: Response) => {
         `https://graph.facebook.com/me?fields=id,name,email&access_token=${accessToken}`
       );
       userData = facebookResponse.data;
+    } else if (provider === "apple") {
+      // Décodage du idToken fourni par Apple
+      const decodedToken = jwt.decode(idToken, { complete: true });
+
+      if (!decodedToken) {
+        Retour.error("Invalid Apple ID token");
+        return res.status(400).json({ message: "Invalid Apple ID token" });
+      }
+
+      const { email, sub: appleUserId } = decodedToken.payload;
+
+      if (!email) {
+        Retour.error("Apple ID token is missing email");
+
+        return res
+          .status(400)
+          .json({ message: "Apple ID token is missing email" });
+      }
+
+      userData = {
+        email,
+        appleUserId,
+        given_name: decodedToken.payload.given_name || "Utilisateur",
+        family_name: decodedToken.payload.family_name || "",
+      };
     } else {
+      Retour.error("Invalid provider");
       return res.status(400).json({ message: "Invalid provider" });
     }
 
@@ -53,7 +81,11 @@ router.post("/socialLogin", async (req: Request, res: Response) => {
           firstname: userData.given_name || userData.name,
           name: userData.family_name || "",
         },
-        picture: { url: userData.picture, public_id: "fromGoogle" },
+        picture: {
+          url: userData.picture || null, // Apple ne fournit pas d'image
+          public_id:
+            "from" + provider.charAt(0).toUpperCase() + provider.slice(1),
+        },
         email: userData.email,
         token: uid2(29),
       });
@@ -66,12 +98,13 @@ router.post("/socialLogin", async (req: Request, res: Response) => {
     }
 
     // Retourner les informations utilisateur et le token
+    Retour.info("Social login successful");
     return res.status(200).json({
       message: "Social login successful",
       customer,
     });
   } catch (error) {
-    console.error("Erreur de connexion sociale :", error);
+    Retour.error("Erreur de connexion sociale");
     return res.status(500).json({
       message: "Une erreur est survenue lors de la connexion sociale",
       error,
